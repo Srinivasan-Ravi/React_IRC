9c763fffbc5e0499c00f4369316a0993059d4e2c
Srinivasan R <sriniravir05@gmail.com>
2023-12-11 12:50:56 +0530

    Day3

--- Day3/myapp/src/App.js
+++ Day3/myapp/src/App.js
@@ -1,15 +1,21 @@
-import Nav from "./Components/Nav";
-import Footer from "./Components/Footer";
-import './assets/css/Nav.css';
+// import Nav from "./Components/Nav";
+// import Footer from "./Components/Footer";
+// import './assets/css/Nav.css';
+import Home from './Components/Home';
+import './assets/css/Home.css';
 function App(){
   return(
+    <body>
     <div>
-      <Nav/>
-    <p>
+      
+      {/* <Nav/> */}
+    <p className='main'>
     Welcome to your own World🌍
     </p>
-    <Footer/>
+    {/* <Footer/> */}
+    <Home/>
     </div>
+    </body>
   )
 }
 export default App
---
+++ Day3/myapp/src/Components/Home.js
@@ -0,0 +1,14 @@
+const Home=()=>
+{
+    return(
+        <div className="home">
+
+        <div className="caption">
+        Explore, Dream, Discover: 
+        </div>
+        <div className="caption">
+        Your Passport to Unforgettable Journeys Begins Here! 🌍✈️</div>
+        </div>
+    )
+}
+export default Home 
