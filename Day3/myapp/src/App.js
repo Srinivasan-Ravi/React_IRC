3cd75470ed4793fc24f22638ed83c4ee6fb022d8
Srinivasan R <sriniravir05@gmail.com>
2023-12-11 16:28:15 +0530

    Day3

--- Day3/myapp/src/App.js
+++ Day3/myapp/src/App.js
@@ -1,20 +1,21 @@
-// import Nav from "./Components/Nav";
-// import Footer from "./Components/Footer";
-// import './assets/css/Nav.css';
+import Nav from "./Components/Nav";
+import './assets/css/Nav.css';
+import Footer  from "./Components/Footer";
 import Home from './Components/Home';
 import './assets/css/Home.css';
+import './assets/css/Footer.css';
 function App(){
   return(
     <body>
     <div>
       
-      {/* <Nav/> */}
     <p className='main'>
     Welcome to your own World🌍
     </p>
-    {/* <Footer/> */}
     <Home/>
+      <Nav/>
     </div>
+    <Footer/>
     </body>
   )
 }
--- Day3/myapp/src/Components/Footer.js
+++ Day3/myapp/src/Components/Footer.js
@@ -1,10 +1,29 @@
-const Footer=()=>{
-    return (
-    <>
-    <div>
-      Srinivasan R-727822TUCS233
-    </div>
-    </>
-    )
-}
+import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <body>
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="social-icons">
+          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+            <FaFacebook />
+          </a>
+          <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+            <FaTwitter />
+          </a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
+          <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+        </div>
+      </div>
+    </footer>
+    </body>
+  );
+};
+
 export default Footer;
